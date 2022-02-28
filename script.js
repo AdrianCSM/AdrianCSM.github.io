e7c63c5b2ea444544713d3fac97322704b878591
Cosmin Adrian <cosminadrian218@gmail.com>
2022-02-28 19:17:41 +0200

    Start mobile menu implementation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function openNav() {
-  document.querySelector(".dropdown").style.width = "100%"
-}
+// function openNav() {
+//   document.querySelector(".dropdown").style.width = "100%"
+// }
 
-function closeNav() {
-  document.querySelector(".dropdown").style.width = "0%"
+// function closeNav() {
+//   document.querySelector(".dropdown").style.width = "0%"
+// }
+
+const mobileMenu = document.querySelector('.nav_menu');
+const navPage = document.querySelector('.nav_page');
+const menuLinks = document.querySelector('.menuMobile');
+
+const openNav = () => {
+    navPage.classList.add('.nav_page');
+    mobileMenu.classList.add('.nav_menu')
 }
