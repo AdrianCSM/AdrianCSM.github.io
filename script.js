b3c138ed0e4e8405990323d90ca97b1e7124ddb1
Lekan <jimoridwanlekan@gmail.com>
2022-02-28 17:44:26 +0100

    add js file

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// This is the mobile menu //
+function openNav() {
+  document.querySelector(".dropdown").style.width = "100%"
+}
+
+function closeNav() {
+  document.querySelector(".dropdown").style.width = "0%"
+}
