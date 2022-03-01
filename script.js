868a79d7c4ea8ea21514e7bab66da09769aa18c6
Cosmin Adrian <cosminadrian218@gmail.com>
2022-03-01 10:49:24 +0200

    Revert "Start styling mobile menu"

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
   document.querySelector(".mobileDrop").style.width = "0%"
 }
+
+// const mobileMenu = document.querySelector('.nav_menu');
+// const navPage = document.querySelector('.nav_page');
+// const menuLinks = document.querySelector('.menuMobile');
+
+// const openNav = () => {
+//     navPage.classList.add('.nav_page');
+//     mobileMenu.classList.add('.nav_menu')
+// }
