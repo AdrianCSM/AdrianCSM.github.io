196cee8a12039724018dde058d0f6d0f704cd50f
Lekan <jimoridwanlekan@gmail.com>
2022-03-01 16:33:59 +0100

    add javascript functions

--- script.js
+++ script.js
@@ -27,3 +27,62 @@ menuLinks.forEach((menuLink) => {
 });
 
 // Modal Pop-Up
+const projectSectionDom = document.querySelector('.projects');
+const popupModal = document.querySelector('.modal');
+const closeModal = document.querySelector('.cross');
+const nameDom = document.querySelector('.modalTitle');
+const descriptionDom = document.querySelector('.modalDescContainer');
+const technologiesDom = document.querySelector('.modalTags');
+const sourceLinkDom = document.querySelector('.btn_view_src');
+const demoLinkDom = document.querySelector('.btnDemo');
+const companyNameModal = document.querySelector('.modalCompany');
+const roleNameModal = document.querySelector('.modalRole');
+const yearDateModal = document.querySelector('.modalYear');
+const projectImageLinkDom = document.querySelector('.viewProject');
+
+const projectsArray = [
+  {
+    title: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snaphhot.svg',
+    tags: ['html', 'css', 'javascript'],
+    role: 'Full Stack Dev',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: 'https://github.com/harlexkhal/Microverse-Porfolio-Website',
+    demoLink: 'https://harlexkhal.github.io/Microverse-Porfolio-Website/',
+  },
+  {
+    title: 'Mulit-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snapshot2.svg',
+    tags: ['html', 'css', 'javascript', 'Ruby on rails'],
+    role: 'Uber',
+    company: 'Lead Developer',
+    year: '2018',
+    sourceLink: 'https://github.com/harlexkhal/Microverse-Porfolio-Website',
+    demoLink: 'https://harlexkhal.github.io/Microverse-Porfolio-Website/',
+  },
+  {
+    title: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snapshot3.svg',
+    tags: ['html', 'css', 'javascript'],
+    role: 'Full Stack Dev',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: 'https://github.com/harlexkhal/Microverse-Porfolio-Website',
+    demoLink: 'https://harlexkhal.github.io/Microverse-Porfolio-Website/',
+  },
+  {
+    title: 'Mulit-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'images/Snapshot4.svg',
+    tags: ['html', 'css', 'javascript', 'Ruby on rails'],
+    role: 'Back End Dev',
+    company: 'Canopy',
+    year: '2015',
+    sourceLink: 'https://github.com/harlexkhal/Microverse-Porfolio-Website',
+    demoLink: 'https://harlexkhal.github.io/Microverse-Porfolio-Website/',
+  },
+];
