f2d3c46e42efeac65621766cf01f30b8b4d3a894
Cosmin Adrian <cosminadrian218@gmail.com>
2022-03-02 15:17:45 +0200

    Add form validation

--- script.js
+++ script.js
@@ -149,3 +149,17 @@ closeModal.addEventListener('click', () => {
   logo.classList.remove('blur');
   document.body.style.overflowY = 'scroll';
 });
+
+// Form validation //
+
+const contactForm = document.querySelector('.contactForm');
+const emailImput = document.querySelector('.emailAddress');
+const validationMessage = document.querySelector('.validationMessage');
+
+contactForm.addEventListener('submit', (event) => {
+  const stringMessage = emailImput.value;
+  if (/[A-Z]/.test(stringMessage)) {
+    validationMessage.innerHTML = 'Your email is supposed to be lower cased.';
+    event.preventDefault();
+  }
+});
