d5ae7b022a585b3d6294031f2bb46b8f66ae1696
Cosmin Adrian <cosminadrian218@gmail.com>
2022-03-02 15:58:33 +0200

    Change styling

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Mobile menu //
 
-const menuIcon = document.querySelector('.nav_menu');
-const sideNav = document.querySelector('.mobileNav');
-const menuLinks = document.querySelectorAll('.menuMobile');
-const mobileMenu = document.querySelector('.parent');
-const logo = document.querySelector('.logo');
+// const menuIcon = document.querySelector('.nav_menu');
+// const sideNav = document.querySelector('.mobileNav');
+// const menuLinks = document.querySelectorAll('.menuMobile');
+// const mobileMenu = document.querySelector('.parent');
+// const logo = document.querySelector('.logo');
 
-const openMenu = () => {
-  sideNav.classList.add('mobileNavActive');
-  menuIcon.classList.add('vanish');
-  mobileMenu.classList.add('blur');
-  logo.classList.add('blur');
-};
+// const openMenu = () => {
+//   sideNav.classList.add('mobileNavActive');
+//   menuIcon.classList.add('vanish');
+//   mobileMenu.classList.add('blur');
+//   logo.classList.add('blur');
+// };
 
-const closeMenu = () => {
-  sideNav.classList.remove('mobileNavActive');
-  mobileMenu.classList.remove('blur');
-  menuIcon.classList.remove('vanish');
-  logo.classList.remove('blur');
-};
+// const closeMenu = () => {
+//   sideNav.classList.remove('mobileNavActive');
+//   mobileMenu.classList.remove('blur');
+//   menuIcon.classList.remove('vanish');
+//   logo.classList.remove('blur');
+// };
 
-menuIcon.addEventListener('click', openMenu);
-menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener('click', closeMenu);
-});
+// menuIcon.addEventListener('click', openMenu);
+// menuLinks.forEach((menuLink) => {
+//   menuLink.addEventListener('click', closeMenu);
+// });
 
 // // Modal Pop-Up and Dynamic Load //
 // const projectSectionDom = document.querySelector('.projects');
